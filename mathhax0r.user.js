1f7d87f98a1b292e0ac24dd64f75ea1fca404623
hiccup01 <hiccup@hiccup01.com>
2016-05-08 08:13:54 +1200

    trying harder

--- mathhax0r.user.js
+++ mathhax0r.user.js
@@ -5,36 +5,48 @@
 // @version     1
 // @grant       none
 // @require     https://raw.githubusercontent.com/Hardmath123/nearley/master/lib/nearley.js
+// @require     https://raw.githubusercontent.com/hiccup01/mathhax0r/master/grammar.js
 // ==/UserScript==
-console.log("Running");
-document.body.getElementsByClassName("questions-text-alignment")[0].innerHTML.split("=")[0].replace(/\s+/g, ''); //Get question
-// This is an example of how to use a nearley-made grammar.
-var PROMPT = "> ";
-var nearley = require("../../lib/nearley.js");
-var grammar = require("./grammar.js");
-
-// This is where the action is.
-function runmath(s) {
-    var ans;
-    try {// We want to catch parse errors and die appropriately
-
-        // Make a parser and feed the input
-        ans = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
-            .feed(s);
-        
-        // Check if there are any results
-        if (ans.results.length) {
-            return ans.results[0].toString();
-        } else {
-            // This means the input is incomplete.
-            var out = "Error: incomplete input, parse failed. :(";
-            return out;
-        }
-    } catch(e) {
-        // Panic in style, by graphically pointing out the error location.
-        var out = new Array(PROMPT.length + e.offset + 1).join("-") + "^  Error.";
-        //                                  --------
-        //                                         ^ This comes from nearley!
-        return out;
-    }
+//console.log("Running");
+/*var question;
+var answer;
+question = document.body.getElementsByClassName("questions-text-alignment")[0].innerHTML.split("=")[0].replace(/\s+/g, ''); //Get question
+function evaluate(expression) {
+                    var p = new nearley.Parser(arithmetic.ParserRules, arithmetic.ParserStart)
+                    try {
+                        p.feed(expression); //Input
+                        if (!p.results[0]) {
+                            throw new Error();
+                        }
+                        if (isNaN(p.results[0]) || p.results[0] === Infinity) {
+                            throw new Error();
+                        }
+                        return p.results[0].toString(); //Output
+                    } catch(e) {
+                        console.log("Something bad happened... :" + e);
+                    }
+                }
+function type(text) {
+document.body.getElementsByClassName("questions-input-adjustment")[0].value = text;
+$('questions-input-adjustment')
+  .trigger({
+    type: 'keypress',
+    which: "\u0013".charCodeAt(0)      
+   });
 }
+
+function addStuff(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "https://raw.githubusercontent.com/Hardmath123/nearley/master/lib/nearley.js");
+  document.body.appendChild(script);
+  script = document.createElement("script");
+  script.setAttribute("src", "https://raw.githubusercontent.com/hiccup01/mathhax0r/master/grammar.js");
+  document.body.appendChild(script);
+}
+addStuff(function() {
+	answer = evaluate(question);
+	console.log(answer);
+});*/
+(function() {
+	alert(typeof module)
+});
